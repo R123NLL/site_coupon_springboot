db9edcce9fa5151993b01a0ca8b1c254503b8869
Denis Shatik <denisshatik@gmail.com>
2024-05-13 00:12:24 +0300

    repository first push

--- src/main/java/src/springboot/dao/CompanyRepository.java
+++ src/main/java/src/springboot/dao/CompanyRepository.java
@@ -1,6 +1,9 @@
 package src.springboot.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import src.springboot.entities.Company;
 
@@ -8,5 +11,9 @@ import java.util.List;
 
 @Repository
 public interface CompanyRepository extends CrudRepository<Company, Long> {
+    boolean existsByEmailAndPassword(String email, String password);
+    boolean existsByName(String name);
+    boolean existsByEmail(String email);
     List<Company> findAll();
+
 }
--- src/main/java/src/springboot/dao/CouponRepository.java
+++ src/main/java/src/springboot/dao/CouponRepository.java
@@ -1,12 +1,30 @@
 package src.springboot.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import src.springboot.entities.Category;
 import src.springboot.entities.Coupon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface CouponRepository extends CrudRepository<Coupon, Long> {
+    Coupon findById(int couponID);
+
+    boolean existsByTitle(String title);
+
+    boolean existsByTitleAndCompanyId(String title, int companyId);
+
+    boolean existsByIdAndCompanyId(int companyId, int couponId);
+    ArrayList<Coupon> findAllByCompanyId(int companyId);
+
+    ArrayList<Coupon> findAllByCompanyIdAndCategory(int companyId, Category category);
+
+    ArrayList<Coupon> findAllByCompanyIdAndPriceLessThanEqual(int companyId, double price);
     List<Coupon> findAll();
+
 }
