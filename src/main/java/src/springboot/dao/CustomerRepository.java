eafb785d382db70facc58756fdd29a408d4c91bc
Denis Shatik <denisshatik@gmail.com>
2024-05-15 19:11:53 +0300

    customer repository added

--- src/main/java/src/springboot/dao/CustomerRepository.java
+++ src/main/java/src/springboot/dao/CustomerRepository.java
@@ -8,5 +8,9 @@ import java.util.List;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    boolean existsCustomerByEmailAndPassword(String email, String password);
+    boolean existsCustomerByEmail(String email);
+    boolean existsCustomerByEmailAndIdIsNot(String email,int id);
     List<Customer> findAll();
+
 }
